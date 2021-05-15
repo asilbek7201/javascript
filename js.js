ca50877da36f230ffd241ac2f538486386beb5b4
asilbek7201 <asilbek.s.2006@gmail.com>
2021-05-15 15:08:18 +0500

    savol

---
+++ js.js
@@ -0,0 +1,120 @@
+// alert("HUSH KELIBSIZ JANOB ASILBEK")
+
+// alert("salom")
+
+// alert(3+2+1)
+
+// let ism="ASILBEK"
+
+// alert(ism)
+
+// ism="BAYOT"
+
+// alert(ism)
+
+// let yosh = 19
+
+// alert(yosh)
+
+// let hello = "Hello World"
+
+// let message
+
+// message = hello
+
+// alert(message)
+
+// const ism = "ASLIDDIN"
+
+// ism = "Asilbek"
+
+// alert(ism)
+
+// let user = "name"
+
+// let email
+
+// email = user
+
+// alert(email)
+
+// let ism 
+
+// ism = prompt("ismingiz nima?")
+
+// alert(`HUSH KELIPSIZ ${ism}`)
+
+// alert(685+1250)
+// alert(1250-685)
+// alert(625*1250)
+// alert(1000/200)
+// alert(10%8)
+// alert(10**2)
+// alert(625**(1/2))
+
+// alert(2>1)
+
+// console.log(3>1)
+
+// alert("z" > "Z")
+
+// let soat = prompt("Soat nechchi bo'ldi?")
+
+// if (soat == 6) {
+//     alert("Ish tugadi")
+// }
+
+// let soat = prompt("2*2 nechchi bo'ladi?")
+
+// if (soat == 4) {
+//     alert("OFARIN")
+// } else {
+//     alert("Notog'ri")
+// }
+
+// let savol = prompt("2*2 nechchi")
+
+// if (savol > 4) {
+//     alert("o'tib kettiz")
+// } else if (savol < 4) {
+//     alert("Qo'shing yana")
+// } else {
+//     alert("to'g'ri")
+// }
+
+// dars
+
+// let havo = prompt("bugun pagoda gradusi nechchi?")
+
+// if (havo > 10) {
+//     alert("iliq")
+// } else if (havo > 15) {
+//     alert("iliqroq")
+// } else if (havo > 20) {
+//     alert("issiqroq")
+// } else if (havo > 30) {
+//     alert("issiq")
+// } else {
+//     alert("bo'ldi")
+// }
+
+let hafta = prompt("bugun haftani qaysi kuni?")
+
+if (hafta == "dushanba") {
+    alert("o'qish kuni")
+} else if (hafta == "seshanba") {
+    alert("adabiyot darsi bor bugun")
+} else if (hafta == "chorshanba") {
+    alert("algebra darsi")
+} else if (hafta == "payshanba") {
+    alert("trinirovka kuni")
+} else if (hafta == "juma") {
+    alert("jismoniy tarbiya darsi")
+} else if (hafta == "shanba") {
+    alert("dars kuni")
+} else if (hafta == "yakshanba") {
+    alert("dam olish kunlari")
+} else {
+    alert("iltimos hafta kunlarini kichkina harflar bilan kiriting")
+}
+
